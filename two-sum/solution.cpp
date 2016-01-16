69b37d3885b03816f868c94719915a0f3fe52dd1
Yudu Ban <banyudu@gmail.com>
2016-01-16 18:13:36 +0800

    optimize two-sum question & modify make file

--- two-sum/solution.cpp
+++ two-sum/solution.cpp
@@ -5,21 +5,36 @@ public:
         vector<int> ret;
         int size = nums.size();
 		unordered_map<int, int> value2index;
+		int cur, another;
 		for (int i = 1; i <= size; i++)
 		{
-			int value = nums[i - 1];
-			if (value2index.find(value) == value2index.end())
+			cur = nums[i - 1];
+			if (value2index.find(cur) == value2index.end())
 			{
-				value2index[value] = i;
+				another = target - cur;
+				if (another != cur)
+				{
+					auto ptr = value2index.find(another);
+					if (ptr != value2index.end())
+					{
+						int v = ptr->second;
+						if (i > v)
+							std::swap(v, i);
+						ret.push_back(i);
+						ret.push_back(v);
+						return ret;
+					}
+				}
+				value2index[cur] = i;				
 			}
-			else if (value + value == target)
+			else if (cur + cur == target)
 			{
-				ret.push_back(value2index[value]);
+				ret.push_back(value2index[cur]);
 				ret.push_back(i);
 				return ret;
 			}
 		}
-		int cur, another;
+
 		for (auto item: value2index)
 		{
 			cur = item.first;
