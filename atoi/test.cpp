446f22e51d2dcb71da43fbb2cd6fddcea21f61da
banyudu <banyudu@gmail.com>
2016-02-29 16:31:46 +0800

    add basic structure for atoi

---
+++ atoi/test.cpp
@@ -0,0 +1,21 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_COLOUR_NONE
+#include <catch.hpp>
+#include "solution.cpp"
+
+Solution sln;
+
+TEST_CASE("normal values", "[Solution]")
+{
+
+}
+
+TEST_CASE("zero", "[Solution]")
+{
+
+}
+
+TEST_CASE("negative values", "[Solution]")
+{
+
+}
