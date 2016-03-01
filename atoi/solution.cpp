70eee570b210deaddd498ca08f64f813451d8c13
banyudu <banyudu@gmail.com>
2016-03-01 13:57:03 +0800

    add solution and test cases for "atoi" problem

--- atoi/solution.cpp
+++ atoi/solution.cpp
@@ -1,6 +1,34 @@
+#include "../common.h"
+
 class Solution {
 public:
     int myAtoi(string str) {
-        
+		const int error_value = 0;
+		// error checks
+		int len = str.length();
+		if (0 == len)
+			return error_value;
+		long long ret = 0;
+		int index = 0;
+		bool bNegative = false;
+		if ('+' == str[0] || '-' == str[0])
+		{
+			index = 1;
+			bNegative = '-' == str[0];
+		}
+
+		// loop through index to len
+		int v;
+		for (int i = index; i < len; i++)
+		{
+			v = str[i] - '0';
+			if (v < 0 || v > 9)
+				return bNegative? -ret: ret;
+			ret *= 10;
+			ret += v;
+			if (ret > INT_MAX)
+				return error_value;
+		}
+		return bNegative? -ret: ret;
     }
 };
--- atoi/test.cpp
+++ atoi/test.cpp
@@ -7,15 +7,69 @@ Solution sln;
 
 TEST_CASE("normal values", "[Solution]")
 {
-
+	REQUIRE(sln.myAtoi("1") == atoi("1"));
+	REQUIRE(sln.myAtoi("2") == atoi("2"));
+	REQUIRE(sln.myAtoi("10") == atoi("10"));
+	REQUIRE(sln.myAtoi("9876") == atoi("9876"));
+	REQUIRE(sln.myAtoi("123456") == atoi("123456"));
 }
 
 TEST_CASE("zero", "[Solution]")
 {
+	REQUIRE(sln.myAtoi("0") == atoi("0"));
+}
 
+TEST_CASE("empty", "[Solution]")
+{
+	REQUIRE(sln.myAtoi("") == atoi(""));
 }
 
 TEST_CASE("negative values", "[Solution]")
 {
+	REQUIRE(sln.myAtoi("-1") == atoi("-1"));
+	REQUIRE(sln.myAtoi("-2") == atoi("-2"));
+	REQUIRE(sln.myAtoi("-10") == atoi("-10"));
+	REQUIRE(sln.myAtoi("-9876") == atoi("-9876"));
+	REQUIRE(sln.myAtoi("-123456") == atoi("-123456"));
+}
 
+TEST_CASE("symbols", "[Solution]")
+{
+	REQUIRE(sln.myAtoi("-20") == atoi("-20"));
+	REQUIRE(sln.myAtoi("+2") == atoi("+2"));
+	REQUIRE(sln.myAtoi("++10") == atoi("++10"));
+	REQUIRE(sln.myAtoi("--9876") == atoi("--9876"));
+	REQUIRE(sln.myAtoi("--123456") == atoi("--123456"));
+	REQUIRE(sln.myAtoi("+0") == atoi("+0"));
+	REQUIRE(sln.myAtoi("-0") == atoi("-0"));
+	REQUIRE(sln.myAtoi("++") == atoi("++"));
+	REQUIRE(sln.myAtoi("-") == atoi("-"));
+	REQUIRE(sln.myAtoi("+") == atoi("+"));
+	REQUIRE(sln.myAtoi("--") == atoi("--"));
+}
+
+TEST_CASE("float values", "[Solution]")
+{
+	REQUIRE(sln.myAtoi(".1") == atoi(".1"));
+	REQUIRE(sln.myAtoi("1.2") == atoi("1.2"));
+	REQUIRE(sln.myAtoi("12345.6789") == atoi("12345.6789"));
+	REQUIRE(sln.myAtoi("+0.1") == atoi("+0.1"));
+	REQUIRE(sln.myAtoi("-9.712") == atoi("-9.712"));
+	REQUIRE(sln.myAtoi("-.1") == atoi("-.1"));
+	REQUIRE(sln.myAtoi("+.2") == atoi("+.2"));
+	REQUIRE(sln.myAtoi(".") == atoi("."));
+	REQUIRE(sln.myAtoi("0.") == atoi("0."));
+	REQUIRE(sln.myAtoi("h") == atoi("h"));
+}
+
+TEST_CASE("bad values", "[Solution]")
+{
+	REQUIRE(sln.myAtoi("h") == atoi("h"));
+	REQUIRE(sln.myAtoi("1a") == atoi("1a"));
+	REQUIRE(sln.myAtoi("a1") == atoi("a1"));
+	REQUIRE(sln.myAtoi(".ui") == atoi(".ui"));
+	REQUIRE(sln.myAtoi("{}") == atoi("{}"));
+	REQUIRE(sln.myAtoi("()") == atoi("()"));
+	REQUIRE(sln.myAtoi("1+2") == atoi("1+2"));
+	REQUIRE(sln.myAtoi("_1") == atoi("_1"));
 }
