9bef475367be3ead24fdf02840d4af05e9dfffa4
banyudu <banyudu@gmail.com>
2016-03-11 16:02:33 +0800

    add solution(not completed)

--- basic-calculator/solution.cpp
+++ basic-calculator/solution.cpp
@@ -3,6 +3,44 @@
 class Solution {
 public:
     int calculate(string s) {
+		// use stack as container
+		stack values;
+		stack operators;
+		int curVal = 0; // current value
 
+		// loop the expression s
+		for (char ch: s)
+		{
+			switch(ch)
+			{
+				// normal digits
+			case '0':
+			case '1':
+			case '2':
+			case '3':
+			case '4':
+			case '5':
+			case '6':
+			case '7':
+			case '8':
+			case '9':
+				curVal *= 10;
+				curVal += ch - '0';
+				break;
+				// operators
+			case '+':
+			case '-':
+
+				// brackets
+			case '(':
+			case ')':
+
+				// spaces
+			case ' ':
+
+				// others
+			default:
+			}
+		}
     }
 };
