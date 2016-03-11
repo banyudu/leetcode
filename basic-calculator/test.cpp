156324132342bb849dee64e4f904b22da74e6332
banyudu <banyudu@gmail.com>
2016-03-11 15:31:44 +0800

    add some test cases

--- basic-calculator/test.cpp
+++ basic-calculator/test.cpp
@@ -5,6 +5,8 @@
 
 Solution sln;
 
+const int INVALID_RESULT = 0;
+
 TEST_CASE("normal values", "[Solution]")
 {
 	REQUIRE(sln.calculate("1+2") == 3);
@@ -12,6 +14,11 @@ TEST_CASE("normal values", "[Solution]")
 	REQUIRE(sln.calculate("100-2+23") == 121);
 	REQUIRE(sln.calculate("2-23+100") == 79);
 	REQUIRE(sln.calculate("2-(23+100)") == -121);
+	REQUIRE(sln.calculate("1+(2+(3+(4+(5+(6+(7+(8)))))))") == 36);
+	REQUIRE(sln.calculate("100 - (1+(2+(3+(4+(5+(6+(7+(8))))))))") == 64);
+	REQUIRE(sln.calculate("123") == 123);
+	REQUIRE(sln.calculate("(123)") == 123);
+	REQUIRE(sln.calculate("(1+2)") == 3);
 }
 
 TEST_CASE("spaces", "[Solution]")
@@ -19,4 +26,22 @@ TEST_CASE("spaces", "[Solution]")
 	REQUIRE(sln.calculate("1+ 2") == 3);
 	REQUIRE(sln.calculate(" 100 +2-23 ") == 79);
 	REQUIRE(sln.calculate("100            -2                               +23") == 121);
+}
+
+TEST_CASE("empty", "[Solution]")
+{
+	REQUIRE(sln.calculate("") == 0);
+	REQUIRE(sln.calculate(" ") == 0);
+	REQUIRE(sln.calculate("                                     ") == 0);
+}
+
+
+TEST_CASE("invalid input", "[Solution]")
+{
+	REQUIRE(sln.calculate("a + 1") == INVALID_RESULT);
+	REQUIRE(sln.calculate("hello world") == INVALID_RESULT);
+	REQUIRE(sln.calculate("1a + 2") == INVALID_RESULT);
+	REQUIRE(sln.calculate("(1+1") == INVALID_RESULT);
+	REQUIRE(sln.calculate(")1+1") == INVALID_RESULT);
+	REQUIRE(sln.calculate(",1+1") == INVALID_RESULT);
 }
