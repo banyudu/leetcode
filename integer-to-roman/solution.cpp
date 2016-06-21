e187c40c3024907357199ed3c9b77bd4570f8475
banyudu <banyudu@gmail.com>
2016-06-21 15:44:31 +0800

    add solution for integer to roman

---
+++ integer-to-roman/solution.cpp
@@ -0,0 +1,28 @@
+#include "../common.h"
+class Solution {
+public:
+	string intToRoman(int num) {
+		const vector<vector<string>> romanValues =
+			{
+				{"I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"},
+				{"X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"},
+				{"C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"},
+				{"M", "MM", "MMM"}
+			};
+		if (num < 1 || num > 3999)
+			return "";
+		string result;
+		int level = 0;
+		while (num > 0)
+		{
+			int mod = num % 10 - 1;
+			if (mod >= 0)
+			{
+				result = romanValues[level][mod] + result;
+			}
+			num /= 10;
+			level++;
+		}
+		return result;
+	}
+};
