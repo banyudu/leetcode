f1201c2592ee86a8eb1f70c6d5c39c724f869f07
Yudu Ban <banyudu@icloud.com>
2019-02-14 07:05:42 +0800

    add solution for 4sum

--- 4sum/solution.cpp
+++ 4sum/solution.cpp
@@ -1,12 +1,56 @@
 #include "../common.h"
 
-class Solution {
-public:
-    vector<vector<int>> fourSum(const vector<int>& nums, int target) {
-        map<int, int> num2Count;
-        for (auto num: nums) {
+class Solution
+{
+  public:
+    vector<vector<int>> fourSum(const vector<int> &nums, int target)
+    {
+        // value to count hash
+        unordered_map<int, int> num2Count;
+        for (auto num : nums)
+        {
             num2Count[num]++;
         }
-        return {};     
+
+        vector<vector<int>> result;
+        unordered_set<string> duplicates;
+
+        // loop through hash
+        for (auto &l1 : num2Count)
+        {
+            l1.second--;
+            for (auto &l2 : num2Count)
+            {
+                if (l2.second)
+                {
+                    l2.second--;
+                    for (auto &l3 : num2Count)
+                    {
+                        if (l3.second)
+                        {
+                            l3.second--;
+                            int toMatch = target - l1.first - l2.first - l3.first;
+                            auto search = num2Count.find(toMatch);
+                            if (search != num2Count.end() && search->second > 0)
+                            {
+                                // find a solution
+                                vector<int> solution = {l1.first, l2.first, l3.first, toMatch};
+                                sort(solution.begin(), solution.end());
+                                auto id = std::to_string(solution[0]) + ',' + std::to_string(solution[1]) + ',' + std::to_string(solution[2]) + ',' + std::to_string(solution[3]);
+                                if (duplicates.find(id) == duplicates.end())
+                                {
+                                    duplicates.insert(id);
+                                    result.push_back(solution);
+                                }
+                            }
+                            l3.second++;
+                        }
+                    }
+                    l2.second++;
+                }
+            }
+            l1.second++;
+        }
+        return result;
     }
 };
--- 4sum/test.cpp
+++ 4sum/test.cpp
@@ -10,7 +10,14 @@ typedef vector<vector<int>> ResultType;
 
 TEST_CASE("normal values", "[Solution]")
 {
-	REQUIRE(Utils::isEqual(sln.fourSum({1, 0, -1, 0, -2, 2}, 0), ResultType({{-1,  0, 0, 1}, {-2, -1, 1, 2}, {-2,  0, 0, 2}})));
+	REQUIRE(Utils::isEqual(sln.fourSum({1, 0, -1, 0, -2, 2}, 0), ResultType({{-1, 0, 0, 1}, {-2, -1, 1, 2}, {-2, 0, 0, 2}})));
+}
+
+TEST_CASE("duplicate values", "[Solution]")
+{
+	REQUIRE(Utils::isEqual(sln.fourSum({1, 1, 1, 1, 1, 1}, 4), ResultType({{1, 1, 1, 1}})));
+	REQUIRE(Utils::isEqual(sln.fourSum({0, 1, 1, 1, 1, 2}, 4), ResultType({{1, 1, 1, 1}, {1, 1, 2, 0}})));
+	REQUIRE(Utils::isEqual(sln.fourSum({0, 1, 1, 1, 1, 2, 0}, 3), ResultType({{1, 1, 1, 0}, {1, 0, 2, 0}})));
 }
 
 TEST_CASE("empty values", "[Solution]")
