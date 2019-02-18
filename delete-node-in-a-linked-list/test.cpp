a2e5b7cd309e6c54bf20b42fea91f9784fc74f2b
Yudu Ban <banyudu@icloud.com>
2019-02-18 20:55:55 +0800

    add new solution

---
+++ delete-node-in-a-linked-list/test.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_COLOUR_NONE
+#include "../catch.hpp"
+#include "../utils.hpp"
+#include "solution.cpp"
+
+Solution sln;
+
+TEST_CASE("normal values", "[Solution]")
+{
+}
