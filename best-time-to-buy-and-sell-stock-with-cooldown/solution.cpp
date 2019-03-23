8c3f3d758a4ffdc9a4f3fbf61e8e3e44a8005053
Yudu Ban <banyudu@icloud.com>
2019-03-23 17:59:49 +0800

    add new solution(copied)

--- best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown/solution.cpp
@@ -1,27 +1,31 @@
 #include "../common.h"
 
+/* copied from https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-cooldown/discuss/75957/An-8ms-C%2B%2B-DP-solution-easy-to-understand */
+
 class Solution
 {
   public:
-    int maxProfit(const vector<int> &prices)
+    int maxProfit(const vector<int> &p)
     {
-        if (prices.size() <= 1)
+        if (p.size() <= 1)
         {
             return 0;
         }
-        int result = prices[1] - prices[0];
-        int last[3] = {0, 0, 0};
-        bool used = false;
-        for (int i = 1; i < prices.size(); i++)
+        int i, sz = p.size();
+        int ret = 0;
+        vector<int> buy(sz, 0);
+        vector<int> sell(sz, 0);
+        buy[0] = -p[0];
+        for (i = 1; i < sz; ++i)
         {
-            int diff = prices[i] - prices[i - 1];
-            int maxValueWithCur = diff + (used ? last[(i + 2) % 3] : last[i % 3]);
-            last[i % 3] = max(maxValueWithCur, last[(i + 2) % 3]);
-            used = maxValueWithCur == last[i % 3];
-            result = max(result, last[i % 3]);
-            cout << "cur: " << prices[i] << ", result: " << result << ", used: " << used << endl;
+            sell[i] = max(buy[i - 1] + p[i], sell[i - 1] - p[i - 1] + p[i]);
+            if (ret < sell[i]) //record the max sell[i]
+                ret = sell[i];
+            if (1 == i)
+                buy[i] = buy[0] + p[0] - p[1];
+            else
+                buy[i] = max(sell[i - 2] - p[i], buy[i - 1] + p[i - 1] - p[i]);
         }
-        cout << endl << "============" << endl;
-        return result > 0 ? result : 0;
+        return ret;
     }
 };
--- best-time-to-buy-and-sell-stock-with-cooldown/test.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown/test.cpp
@@ -11,4 +11,6 @@ TEST_CASE("normal values", "[Solution]")
     REQUIRE(sln.maxProfit({1,2,4}) == 3);
     REQUIRE(sln.maxProfit({6,1,6,4,3,0,2}) == 7);
     REQUIRE(sln.maxProfit({6,1,3,2,4,7}) == 6);
+    REQUIRE(sln.maxProfit({1,7,2,4,11}) == 13);
+    REQUIRE(sln.maxProfit({10,7,2,4,1,2,3,4,5,6,7,8,9,20}) == 20);
 }
