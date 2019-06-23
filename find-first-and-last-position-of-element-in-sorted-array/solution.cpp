0285bbb259d31507061c6ab5d8a7cdaffbece8db
Yudu Ban <banyudu@icloud.com>
2019-06-24 00:55:58 +0800

    add new solution

---
+++ find-first-and-last-position-of-element-in-sorted-array/solution.cpp
@@ -0,0 +1,48 @@
+#include "../common.h"
+
+class Solution {
+public:
+    vector<int> searchRange(const vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+        if (nums[left] == target || nums[right] == target) {
+            return { this->findEdge(nums, target, left, right, true), this->findEdge(nums, target, left, right, false) };
+        }
+        while (left < right - 1) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return { this->findEdge(nums, target, left, mid, true), this->findEdge(nums, target, mid, right, false) };
+            }
+            else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return { -1, -1 };
+    }
+
+    int findEdge (const vector<int>& nums, int target, int left, int right, bool findLeft) {
+        if (nums[left] == nums[right]) {
+            return findLeft ? left : right;
+        }
+        while (left < right - 1) {
+            cout << "left: " << left << ", right: " << right << endl;
+            int mid = (left + right) / 2;
+            if (nums[mid] < target) {
+                left = mid;
+            } else if (nums[mid] > target) {
+                right = mid;
+            } else {
+                if (findLeft) {
+                    right = mid;
+                } else {
+                    left = mid;
+                }
+            }
+        }
+        return findLeft
+            ? nums[left] == target ? left : left + 1
+            : nums[right] == target ? right:  right - 1;
+    }
+};
---
+++ find-first-and-last-position-of-element-in-sorted-array/test.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_COLOUR_NONE
+#include "../catch.hpp"
+#include "solution.cpp"
+#include "../utils.hpp"
+
+Solution sln;
+
+TEST_CASE("normal values", "[Solution]")
+{
+    REQUIRE(Utils::isEqual(sln.searchRange({5,7,7,8,8,10}, 8), {3, 4}));
+    REQUIRE(Utils::isEqual(sln.searchRange({5,7,7,8,10}, 8), {3, 3}));
+    REQUIRE(Utils::isEqual(sln.searchRange({5,7,7,10}, 8), {-1, -1}));
+}
