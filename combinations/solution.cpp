eca96ecd96c6599f5566449b4b02cdac2d1001c7
Yudu Ban <banyudu@gmail.com>
2019-06-29 12:35:37 +0800

    add new solution

--- combinations/solution.cpp
+++ combinations/solution.cpp
@@ -1,1 +1,29 @@
 #include "../common.h"
+
+class Solution {
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> result;
+        vector<int> staging;
+        this->combine(n, k, staging, result);
+        return result;
+    }
+
+    void combine(int n, int k, vector<int> &staging, vector<vector<int>> &result) {
+       if (static_cast<int>(staging.size()) >= k) {
+           result.push_back(staging);
+           return;
+       }
+       if (n < 1) {
+           return;
+       }
+
+       // without current element
+       this->combine(n - 1, k, staging, result);
+
+       // with current element
+       staging.push_back(n);
+       this->combine(n - 1, k, staging, result);
+       staging.pop_back();
+    }
+};
--- combinations/test.cpp
+++ combinations/test.cpp
@@ -1,10 +1,19 @@
 #define CATCH_CONFIG_MAIN
 #define CATCH_CONFIG_COLOUR_NONE
 #include "../catch.hpp"
+#include "../utils.hpp"
 #include "solution.cpp"
 
 Solution sln;
 
 TEST_CASE("normal values", "[Solution]")
 {
+    REQUIRE(Utils::isEqual(sln.combine(4, 2), {
+        {2,4},
+        {3,4},
+        {2,3},
+        {1,2},
+        {1,3},
+        {1,4}
+    }));
 }
